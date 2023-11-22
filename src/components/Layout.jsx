56dd60ee406bd4d525548d0c3ab6ebc6cc1408db
Nahuel <salvatierra.nahuel@gmai.com>
2023-11-22 19:43:05 -0300

    Add common components in Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./navbar/NavBar";
+import Footer from "./footer/Footer";
+import CardProduct from "./cards/CardProduct";
+
+export default function Layout() {
+	return (
+		<>
+			<header>
+				<NavBar />
+			</header>
+
+			<main>
+				<Outlet />
+				<br />
+				<div className="container mx-auto flex flex-wrap gap-5" >
+					<CardProduct />
+					<CardProduct />
+					<CardProduct />
+					<CardProduct />
+					<CardProduct />
+				</div>
+			</main>
+			<footer>
+				<Footer />
+			</footer>
+		</>
+	);
+}
