9e89666ceb1c40cde0ad65b8304b9dbe7f74f0eb
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-11-24 02:55:36 -0300

    feat: :sparkles: Prueba de fetch Product
    He añadido a modo de prueba el fetch para producto de una Api de google, luego será modificada para usar con backend

---
+++ src/components/cards/CardProduct.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const CardProduct = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get("https://api.escuelajs.co/api/v1/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.log("Mostramos el error: -->   ", error);
+      }
+    };
+    fetchProduct();
+  }, []);
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="card w-96 bg-base-100 shadow-xl border m-3 mb-10"
+        >
+          <figure>
+            <img src={product.images} className="w-80" alt={product.title} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title"> {product.title} </h2>
+            <p> {product.category.name} </p>
+            <p> {product.description} </p>
+            <div className="card-actions justify-between items-center">
+              <p> {product.price} </p>
+              <button className="btn btn-primary"> Comprar </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardProduct;
