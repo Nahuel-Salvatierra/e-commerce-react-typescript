8c9d4f738f74d6384d6fc49dc96dc4f894a3ab37
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-12-06 16:29:38 -0300

    feat: :zap: Funcion para obtener datos de las categorias agregadas al servidor
    Se ha realizado la api para que en el dropdown aparezcan las categorias que se vayan agregando en el servidor. Luego, estas seran renderizadas.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,94 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // Imagenes
 import Logo from "../../assets/logo/animarte-logo.png";
 import OffCanvas from "./navComponent/OffCanvas";
-import Login from "../auth/Login";
 import CartOffCanvas from "./navComponent/contentOffCanvas/CartOffCanvas";
-import useCart from "./../../hook/useCart";
-import Register from './../../components/auth/Register.jsx'
-import LayoutAsk from './../../components/LayoutAsk'
+import LayoutAsk from "./../../components/LayoutAsk";
+// Iconos
+import { IconCart, IconUser } from "../IconHero";
+// Fetch Category
+import { getCategoryName } from "../../api/category.api";
+
 
 const NavBar = () => {
-	const [cartOffcanvasShow, setCartOffcanvasShow] = useState(false);
-	const [userOffcanvasShow, setUserOffcanvasShow] = useState(false);
-	const [checkRegister, setCheckRegister] = useState(true)
+    // Renderizar Offcanvas
+    const [cartOffcanvasShow, setCartOffcanvasShow] = useState(false);
+    const [userOffcanvasShow, setUserOffcanvasShow] = useState(false);
+    //Alternar nombre de titulo OffCanvas
+    const [checkRegister, setCheckRegister] = useState("Register");
 
-	const handleCart = () => {
-		setCartOffcanvasShow(true);
-	};
-	const handleUser = () => {
-		setUserOffcanvasShow(true);
-	};
-	const closeCart = () => {
-		setCartOffcanvasShow(false);
-		setUserOffcanvasShow(false);
-	};
+    const handleCart = () => {
+        setCartOffcanvasShow(true);
+    };
+    const handleUser = () => {
+        setUserOffcanvasShow(true);
+    };
+    const closeCart = () => {
+        setCartOffcanvasShow(false);
+        setUserOffcanvasShow(false);
+    };
 
-	return (
-		<div className="navbar bg-neutral">
-			<div className="flex-1">
-				<a className="ps-5 text-xl">
-					<img
-						src={Logo}
-						alt="logo animarte"
-						className="nav-logo w-[60px] bg-white rounded-[50%]"
-					/>
-				</a>
-			</div>
-			<div className="flex-none">
-				<div className="dropdown dropdown-end flex gap-3">
-					<button onClick={handleCart}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth="1.5"
-							stroke="currentColor"
-							className="w-6 h-6"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-							/>
-						</svg>
-						{cartOffcanvasShow && (
-							<OffCanvas
-								offcanvasTitle={"Carrito de Compras"}
-								offcanvasContent={<CartOffCanvas />}
-								onClose={closeCart}
-								showButton={true}
-							/>
-						)}
-					</button>
-					<button onClick={handleUser}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth="1.5"
-							stroke="currentColor"
-							className="w-6 h-6"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-							/>
-						</svg>
+    useEffect(() => {
+        setCheckRegister((prevTitle) => prevTitle === "Register" ? "Register" : "Login");
+    }, []);
+    
 
-						{userOffcanvasShow && (
-							<OffCanvas
-								offcanvasTitle={checkRegister ? 'Register' : 'Login'}
-								offcanvasContent={<LayoutAsk onClose={closeCart}/>}
-								onClose={closeCart}
-							/>
-						)}
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+    // useEffect(() => {
+    //     const fetchCategory = async () => {
+    //         await getCategoryName();
+    //     }
+    //     fetchCategory();
+    // },[]);
+
+
+
+    return (
+        <div className="navbar bg-neutral">
+            <div className="flex-1">
+                <a className="ps-5 text-xl">
+                    <img
+                        src={Logo}
+                        alt="logo animarte"
+                        className="nav-logo w-[60px] bg-white rounded-[50%]"
+                    />
+                </a>
+            </div>
+            <div className="flex-none">
+                <div className="dropdown dropdown-end flex gap-3">
+                    <button onClick={handleCart}>
+                        {<IconCart />}
+                        {cartOffcanvasShow && (
+                            <OffCanvas
+                                offcanvasTitle={"Carrito de Compras"}
+                                offcanvasContent={<CartOffCanvas />}
+                                onClose={closeCart}
+                                showButton={true}
+                            />
+                        )}
+                    </button>
+                    <button onClick={handleUser}>
+                        {< IconUser />}
+
+                        {userOffcanvasShow && (
+                            <OffCanvas
+                                offcanvasTitle={
+                                    checkRegister
+                                }
+                                offcanvasContent={
+                                    <LayoutAsk onClose={closeCart} />
+                                }
+                                onClose={closeCart}
+                            />
+                        )}
+                    </button>
+                    {/* Categorias de Productos */}
+                    {/* <details className="dropdown">
+                        <summary className="m-1 btn">open or close</summary>
+                        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                            {renderCategory.map((category, index) => (
+                                <li key={index}>{category.category}</li>
+                            ))}
+                        </ul>
+                    </details> */}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
